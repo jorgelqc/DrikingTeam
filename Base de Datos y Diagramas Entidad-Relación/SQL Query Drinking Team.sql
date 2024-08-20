e67837a9403a845fc2128db4479fde16603cd9cc
MacaDiDino <didino.macarena@gmail.com>
2024-08-20 19:15:40 +0200

    modificacion de base de datos

--- Base de Datos y Diagramas Entidad-Relación/SQL Query Drinking Team.sql
+++ Base de Datos y Diagramas Entidad-Relación/SQL Query Drinking Team.sql
@@ -10,23 +10,23 @@ USE DrinkingTeamDB;
 -- Tabla Proveedores
 CREATE TABLE Proveedores (
     ProveedorID INT IDENTITY(1,1) PRIMARY KEY, 
-    Nombre NVARCHAR(50) NOT NULL,  -- En compras_limpio o PurchasesFINAL12312016 Columna "VendorName"
-    NumeroProveedor INT NOT NULL,   -- En compras_limpio o PurchasesFINAL12312016 Columna "VendorNumber"
+    Nombre NVARCHAR(50) NOT NULL,  
+    NumeroProveedor INT NOT NULL,   
 );
 
 -- Tabla Productos
 CREATE TABLE Productos (
     ProductoID INT IDENTITY(1,1) PRIMARY KEY,
-    Marca NVARCHAR(100) NOT NULL,  -- En ventas_limpio o SalesFINAL12312016 Columna "Brand"
-    Nombre NVARCHAR(100) NOT NULL,  -- En ventas_limpio o SalesFINAL12312016 Columna "Description"
-    Tamaño NVARCHAR(100) NOT NULL,   -- En ventas_limpio o SalesFINAL12312016 Columna "Volumne"
+    Marca NVARCHAR(100) NOT NULL,  
+    Nombre NVARCHAR(100) NOT NULL,  
+    Volumen NVARCHAR(100) NOT NULL,   
 );
 
 -- Tabla Tiendas
 CREATE TABLE Tiendas (
     TiendaID INT IDENTITY(1,1) PRIMARY KEY,
-    NumeroTienda NVARCHAR(50) NOT NULL,    -- En Inventario_inicial_limpio o BegInvFINAL12312016 Columna "Store"
-	Ciudad NVARCHAR(50) NOT NULL,       -- En Inventario_inicial_limpio o BegInvFINAL12312016 Columna "City"
+    NumeroTienda NVARCHAR(50) NOT NULL,    
+	Ciudad NVARCHAR(50) NOT NULL,      
 );
 
 -- Tabla Inventario
@@ -34,11 +34,9 @@ CREATE TABLE Inventario (
 	InventarioID INT IDENTITY(1,1) PRIMARY KEY,
     ProductoID INT NOT NULL,
 	TiendaID INT NOT NULL,
-    Precio DECIMAL(10, 2) NOT NULL, -- En Inventario_inicial_limpio o BegInvFINAL12312016 Columna "Price"
-    FechaInicial DATE NOT NULL,     -- En Inventario_inicial_limpio o BegInvFINAL12312016 Columna "startDate"
-	StockInicial INT NOT NULL,       -- En Inventario_inicial_limpio o BegInvFINAL12312016 Columna "onHand"
-	FechaFinal DATE NOT NULL,    -- En Inventario_final_limpio o EndInvFINAL12312016 Columna "startDate"
-	StockFinal INT NOT NULL,     -- En Inventario_final_limpio o EndInvFINAL12312016 Columna "onHand"
+    Precio DECIMAL(10, 2) NOT NULL, 
+    Fecha DATE NOT NULL,     
+	Stock INT NOT NULL,      
 );
 
 -- Tabla Ventas
@@ -46,25 +44,25 @@ CREATE TABLE Ventas (
     VentaID INT IDENTITY(1,1) PRIMARY KEY,
     InventarioID INT NOT NULL,
 	ProveedorID INT NOT NULL,
-    CantidadVendida INT NOT NULL,   -- En ventas_limpio o SalesFINAL12312016 Columna "SalesQuantity"
-    TotalVenta DECIMAL(10, 2) NOT NULL,   -- En ventas_limpio o SalesFINAL12312016 Columna "SalesDollars"
-    PrecioVenta DECIMAL(10, 2) NOT NULL,   -- En ventas_limpio o SalesFINAL12312016 Columna "SalesPrice"
-    FechaVenta DATE NOT NULL,   -- En ventas_limpio o SalesFINAL12312016 Columna "SalesDate"
+    CantidadVendida INT NOT NULL,   
+    TotalVenta DECIMAL(10, 2) NOT NULL,  
+    PrecioVenta DECIMAL(10, 2) NOT NULL,   
+    FechaVenta DATE NOT NULL,  
 );
 
--- Tabla DetallesCompras
-CREATE TABLE DetallesCompras (
+-- Tabla Compras
+CREATE TABLE Compras (
     ComprasID INT IDENTITY(1,1) PRIMARY KEY,
     InventarioID INT NOT NULL,
     ProveedorID INT NOT NULL,
-    NumeroOrdenCompra INT NOT NULL,   -- En compras_limpio o PurchasesFINAL12312016 Columna "PONumber"
-    FechaOrdenCompra DATE NOT NULL, -- En compras_limpio o PurchasesFINAL12312016 Columna "PODate"
-    FechaRecibido DATE NOT NULL,   -- En compras_limpio o PurchasesFINAL12312016 Columna "ReceivingDate"
-    FechaFacturado DATE NOT NULL,  -- En compras_limpio o PurchasesFINAL12312016 Columna "InvoiceDate"
-	FechaPagado DATE NOT NULL,  -- En compras_limpio o PurchasesFINAL12312016 Columna "PayDate"
-    PrecioCompra DECIMAL(10, 2) NOT NULL,   -- En compras_limpio o PurchasesFINAL12312016 Columna "PurchasePrice"
-	Cantidad INT NOT NULL,   -- En compras_limpio o PurchasesFINAL12312016 Columna "Quantity"
-	TotalCompra DECIMAL(10, 2) NOT NULL,   -- En compras_limpio o PurchasesFINAL12312016 Columna "Dollars"
+    NumeroOrdenCompra INT NOT NULL,   
+    FechaOrdenCompra DATE NOT NULL, 
+    FechaRecibido DATE NOT NULL,   
+    FechaFacturado DATE NOT NULL,  
+	FechaPagado DATE NOT NULL,  
+    PrecioCompra DECIMAL(10, 2) NOT NULL,   
+	Cantidad INT NOT NULL,   
+	TotalCompra DECIMAL(10, 2) NOT NULL,   
 );
 -----------------------------------------------------------
 -- CREACION DE RELACIONES ENTRE TABLAS
@@ -90,13 +88,13 @@ ALTER TABLE Ventas
 ADD CONSTRAINT Fk_Ventas_Proveedores -- Conecta las tablas
 FOREIGN KEY (ProveedorID) REFERENCES Proveedores(ProveedorID);
 
--- RELACION ENTRE LA TABLA DE DETALLES COMPRAS E INVENTARIO
-ALTER TABLE DetallesCompras
-ADD CONSTRAINT Fk_DetallesCompras_Inventario -- Conecta las tablas
+-- RELACION ENTRE LA TABLA DE COMPRAS E INVENTARIO
+ALTER TABLE Compras
+ADD CONSTRAINT Fk_Compras_Inventario -- Conecta las tablas
 FOREIGN KEY (InventarioID) REFERENCES Inventario(InventarioID);
 
--- RELACION ENTRE LA TABLA DE DETALLES COMPRAS Y PROVEEDOR
-ALTER TABLE DetallesCompras
-ADD CONSTRAINT Fk_DetallesCompras_Proveedores -- Conecta las tablas
+-- RELACION ENTRE LA TABLA DE COMPRAS Y PROVEEDOR
+ALTER TABLE Compras
+ADD CONSTRAINT Fk_Compras_Proveedores -- Conecta las tablas
 FOREIGN KEY (ProveedorID) REFERENCES Proveedores(ProveedorID);
 
